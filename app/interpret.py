3a11da23dba68dc75fe6d532f1ad9ade3130a3f9
namnatulco <rensvdheijden@gmail.com>
2025-01-30 14:58:40 +0100

    first draft of interpretation code

---
+++ app/interpret.py
@@ -0,0 +1,68 @@
+import instaloader
+from dateparser.search import search_dates
+import spec
+
+
+# this method implements various methods to search for place names
+# returns a list of strings
+def search_place(text):
+    return []
+
+# this method implements various methods to search for organizers
+# returns a list of strings
+def search_organizers(text):
+    return []
+
+# guess event title
+def guess_event_title(text):
+    if not text:
+        return ""
+    return text.split("\n")[0]
+
+# interprets the instaloader post object, assumes wellformed input
+# returns a list of possible spec.Event objects
+def interpret_event_insta(post):
+    
+    text = str(post.caption)
+
+    # identify dates in the caption - will include time
+    possible_dates = search_dates(text, languages=['de','en'])
+
+    possible_locations = []
+    if post.location:
+        possible_locations.append(str(post.location))
+    possible_locations.append(search_place(text))
+
+    event_type = spec.EventTypes.guess_event_type(text)
+
+    event_name = ""
+    if post.title:
+        event_name = str(post.title)
+    else:
+        event_name = guess_event_title(text)
+
+    organizers = search_organizers(text)
+    post_author = str(post.owner_username)
+    if post_author:
+        organizers.append(post_author) # assume author is organizer
+    
+    image = post.url
+
+    post_URL = "https://www.instagram.com/p/" + str(post.shortcode) + "/"
+
+    possible_events = []
+    for eventdate in possible_dates:
+        for loc in possible_locations:
+            ev = spec.Event()
+            ev.date = eventdate #todo
+            ev.location = loc
+            ev.event_type = event_type
+            ev.event_name = event_name
+            ev.organizers = organizers
+            ev.post_author = post_author
+            ev.post_URL = post_URL
+            ev.fulltext = text
+            ev.mediaurls = [image]
+            possible_events.append(ev)
+
+    return possible_events
--- app/spec.py
+++ app/spec.py
@@ -1,29 +1,63 @@
 import json
 
+from enum import Enum
+class EventTypes(Enum):
+    UNKNOWN = 0
+    RALLY = 1
+    DEMONSTRATION = 2
+    CULTURE = 3
+    TALK = 4
+    DISCUSSION = 5
+    WORKSHOP = 6
+    PLENARY = 7
+
+    # guesses the event type based on heuristics
+    # assumes wellformed argument, defaults to UNKNOWN
+    def guess_event_type(text):
+        return EventTypes.UNKNOWN
+
+
 # the only purpose of this is to fix the json spec and handle it consistently
 # there are probably better ways to do this but this felt quickest
 class Event:
 
+    # datetime object or string
     date = None
+
+    # deprecated
     start_time = None
+
+    # string
     location = None
+
+    # spec.EventType
     event_type = None
+
+    # string
     event_name = None
+
+    # list[string]
     organizers = []
+
+    # string
     post_author = None
+
+    # URL as string
     post_URL = None
+
+    # string
+    fulltext = None
+
+    # list[URL as string]
+    mediaurls = []
 
     # empty constructor
     def __init__(self):
         pass
 
-    # empty constructor
-    def __init__(self, jsonblob):
-        self.from_json(jsonblob)
-
     # dump the object to json - right now just a flat list
     def to_json(self):
-        return json.dumps({"date": self.date, "start_time": self.start_time, "location":self.location, "event_type" : self.event_type, "event_name": self.event_name,"organizers":self.organizers,"post_author":self.post_author, "post_URL":self.post_URL})
+        return json.dumps({"date": str(self.date), "start_time": self.start_time, "location":self.location, "event_type" : self.event_type.name, "event_name": self.event_name,"organizers":self.organizers,"post_author":self.post_author, "post_URL":self.post_URL, "fulltext":self.fulltext, "mediaurls":self.mediaurls})
 
     # from JSON to object, no validation
     def from_json(self, jsonblob):
@@ -36,3 +70,8 @@ class Event:
         self.organizers = myjson.organizers
         self.post_author = myjson.post_author
         self.post_URL = myjson.post_URL
+        self.fulltext = myjson.fulltext
+        self.mediaurls = myjson.mediaurls
+
+    def __str__(self):
+        return self.to_json()
